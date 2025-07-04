e071bd96db1640548fe74038f506687a5616b38d
fcurrie <frank@sfle.ca>
2025-07-04 16:56:06 -0400

    Trigger workflow yet again

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, from the final frontier!'
+    return 'Hello, from the final frontier! Again!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
